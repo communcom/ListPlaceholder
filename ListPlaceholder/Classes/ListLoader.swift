18cdfd8bccb4f8698f59c746883b2de57b1d7508
Artem Shilin <shilinartem@gmail.com>
2020-04-20 17:06:09 +0300

    add colors asset

--- ListPlaceholder/Classes/ListLoader.swift
+++ ListPlaceholder/Classes/ListLoader.swift
@@ -36,7 +36,7 @@ import UIKit
             ListLoader.addLoaderToViews([self])
         }
     }
-    
+
     public func hideLoader(){
         self.isUserInteractionEnabled = true
         if self is UITableView{
@@ -62,23 +62,38 @@ import UIKit
 @objc extension UIColor {
     static func backgroundFadedGrey()->UIColor
     {
+        if #available(iOS 11.0, *) {
+            if let color = UIColor(named: "ListLoader.backgroundFadedGrey") {
+                return color
+            }
+        }
         return UIColor(red: (246.0/255.0), green: (247.0/255.0), blue: (248.0/255.0), alpha: 1)
     }
-    
+
     static func gradientFirstStop()->UIColor
     {
+        if #available(iOS 11.0, *) {
+            if let color = UIColor(named: "ListLoader.gradientFirstStop") {
+                return color
+            }
+        }
         return  UIColor(red: (238.0/255.0), green: (238.0/255.0), blue: (238.0/255.0), alpha: 1.0)
     }
-    
+
     static func gradientSecondStop()->UIColor
     {
+        if #available(iOS 11.0, *) {
+            if let color = UIColor(named: "ListLoader.gradientSecondStop") {
+                return color
+            }
+        }
         return UIColor(red: (221.0/255.0), green: (221.0/255.0), blue:(221.0/255.0) , alpha: 1.0);
     }
 }
 
 @objc extension UIView{
     func boundInside(_ superView: UIView){
-        
+
         self.translatesAutoresizingMaskIntoConstraints = false
         superView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[subview]-0-|", options: NSLayoutConstraint.FormatOptions(), metrics:nil, views:["subview":self]))
         superView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[subview]-0-|", options: NSLayoutConstraint.FormatOptions(), metrics:nil, views:["subview":self]))
@@ -103,20 +118,20 @@ extension CGFloat
         views.forEach { $0.ld_addLoader() }
         CATransaction.commit()
     }
-    
+
     static func removeLoaderFromViews(_ views: [UIView])
     {
         CATransaction.begin()
         views.forEach { $0.ld_removeLoader() }
         CATransaction.commit()
     }
-    
+
     public static func addLoaderTo(_ list : ListLoadable )
     {
         self.addLoaderToViews(list.ld_visibleContentViews())
     }
-    
-    
+
+
     public static func removeLoaderFrom(_ list : ListLoadable )
     {
         self.removeLoaderFromViews(list.ld_visibleContentViews())
@@ -127,12 +142,18 @@ extension CGFloat
 {
     override func draw(_ rect: CGRect) {
         super.draw(rect)
+        var bgColor: UIColor = .white
+        if #available(iOS 11.0, *) {
+            if let color = UIColor(named: "ListLoader.backgroundColor") {
+                bgColor = color
+            }
+        }
         let context = UIGraphicsGetCurrentContext()
-        context?.setFillColor(UIColor.white.cgColor)
+        context?.setFillColor(bgColor.cgColor)
         context?.fill(self.bounds)
-        
+
         for view in (self.superview?.subviews)! {
-            
+
             if view != self {
                 context?.setBlendMode(.clear);
                 let rect = view.frame
@@ -144,8 +165,8 @@ extension CGFloat
             }
         }
     }
-    
-    
+
+
     override func layoutSubviews() {
         self.setNeedsDisplay()
         self.superview?.ld_getGradient()?.frame = (self.superview?.bounds)!
@@ -168,49 +189,49 @@ var gradientFirstStop           = 0.1
     {
         return objc_getAssociatedObject(self, &cutoutHandle) as! UIView?
     }
-    
+
     fileprivate func ld_setCutoutView(_ aView : UIView)
     {
         return objc_setAssociatedObject(self, &cutoutHandle, aView, .OBJC_ASSOCIATION_RETAIN)
     }
-    
+
     fileprivate func ld_getGradient()->CAGradientLayer?
     {
         return objc_getAssociatedObject(self, &gradientHandle) as! CAGradientLayer?
     }
-    
+
     fileprivate func ld_setGradient(_ aLayer : CAGradientLayer)
     {
         return objc_setAssociatedObject(self, &gradientHandle, aLayer, .OBJC_ASSOCIATION_RETAIN)
     }
-    
+
     fileprivate func ld_addLoader()
     {
         let gradient: CAGradientLayer = CAGradientLayer()
         gradient.frame = CGRect(x: 0, y: 0, width: self.bounds.size.width , height: self.bounds.size.height)
         self.layer.insertSublayer(gradient, at:0)
-        
+
         self.configureAndAddAnimationToGradient(gradient)
         self.addCutoutView()
     }
-    
+
     fileprivate func ld_removeLoader()
     {
         self.ld_getCutoutView()?.removeFromSuperview()
         self.ld_getGradient()?.removeAllAnimations()
         self.ld_getGradient()?.removeFromSuperlayer()
-        
+
         for view in self.subviews {
             view.alpha = 1
         }
     }
-    
-    
+
+
     func configureAndAddAnimationToGradient(_ gradient : CAGradientLayer)
     {
         gradient.startPoint = CGPoint(x: -1.0 + CGFloat(gradientWidth), y: 0)
         gradient.endPoint = CGPoint(x: 1.0 + CGFloat(gradientWidth), y: 0)
-        
+
         gradient.colors = [
             UIColor.backgroundFadedGrey().cgColor,
             UIColor.gradientFirstStop().cgColor,
@@ -218,43 +239,43 @@ var gradientFirstStop           = 0.1
             UIColor.gradientFirstStop().cgColor,
             UIColor.backgroundFadedGrey().cgColor
         ]
-        
+
         let startLocations = [NSNumber(value: gradient.startPoint.x.doubleValue() as Double),NSNumber(value: gradient.startPoint.x.doubleValue() as Double),NSNumber(value: 0 as Double),NSNumber(value: gradientWidth as Double),NSNumber(value: 1 + gradientWidth as Double)]
-        
-        
+
+
         gradient.locations = startLocations
         let gradientAnimation = CABasicAnimation(keyPath: "locations")
         gradientAnimation.fromValue = startLocations
         gradientAnimation.toValue = [NSNumber(value: 0 as Double),NSNumber(value: 1 as Double),NSNumber(value: 1 as Double),NSNumber(value: 1 + (gradientWidth - gradientFirstStop) as Double),NSNumber(value: 1 + gradientWidth as Double)]
-        
+
         gradientAnimation.repeatCount = Float.infinity
-        gradientAnimation.fillMode = .forwards
+//        gradientAnimation.fillMode = .forwards
         gradientAnimation.isRemovedOnCompletion = false
         gradientAnimation.duration = loaderDuration
         gradient.add(gradientAnimation ,forKey:"locations")
-        
-        
+
+
         self.ld_setGradient(gradient)
-        
+
     }
-    
+
     fileprivate func addCutoutView()
     {
         let cutout = CutoutView()
         cutout.frame = self.bounds
         cutout.backgroundColor = UIColor.clear
-        
+
         self.addSubview(cutout)
         cutout.setNeedsDisplay()
         cutout.boundInside(self)
-        
+
         for view in self.subviews {
             if view != cutout {
                 view.alpha = 0
             }
         }
-        
-        
+
+
         self.ld_setCutoutView(cutout)
     }
 }
